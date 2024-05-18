8bb20e2bc023d1f21c65ba22b91b32788f94a906
danmc400live <danlechuga@live.com>
2024-05-17 19:47:37 -0600

    se agrega estilos y javascript

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+
+@app.route("/sendMail",methods=['POST'])
+def sendMail():
+    msg = ''
+    nombre = request.form['nombre']
+    email = request.form['email']
+    mensaje = request.form['mensaje']
+    return render_template('index.html')
+
